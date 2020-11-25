76a7b6a0ed839d4f24067aa4994999141bc52a4c
Cathrine C <cph-ac221@cphbusiness.dk>
2020-11-25 11:49:19 +0100

    Migration implemented

--- src/main/java/infrastructure/DBUserRepository.java
+++ src/main/java/infrastructure/DBUserRepository.java
@@ -1,12 +1,5 @@
 package infrastructure;
 
-import businessLogic.Bottom;
-import businessLogic.Cupcake;
-import businessLogic.Topping;
-import domain.shoppinglist.NoSuchUserExists;
-import domain.shoppinglist.User;
-import domain.shoppinglist.UserRepository;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
--- src/main/java/infrastructure/Database.java
+++ src/main/java/infrastructure/Database.java
@@ -1,12 +1,15 @@
 package infrastructure;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Database {
-    private final String URL = "jdbc:mysql://localhost:3306/fogtraelast?serverTimezone=CET";
-    private final String USER = "fogtraelast";
+
+    //Credentials
+    private static final String URL = "jdbc:mysql://localhost:3306/fogtraelast?serverTimezone=CET";
+    private static final String USER = "fogtraelast";
+
+    // Database version
+    private static final int version = 0;
 
     public Database() {
         try {
@@ -17,6 +20,31 @@ public class Database {
     }
 
     public Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL, USER, null);
+    }
+
+    public static int getVersion() {
+        return version;
+    }
+
+    public static int getCurrentVersion() {
+        try (Connection conn = getConnection()) {
+            Statement s = conn.createStatement();
+            ResultSet rs = s.executeQuery("SELECT value FROM properties WHERE name = 'version';");
+            if(rs.next()) {
+                String column = rs.getString("value");
+                return Integer.parseInt(column);
+            } else {
+                System.err.println("No version in properties.");
+                return -1;
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            return -1;
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, null);
     }
 
