668ce1688c73a70b9f53784bf562afba1c7386c1
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2020-11-29 13:31:33 +0100

    added api

---
+++ src/main/java/api/FogTraelast.java
@@ -0,0 +1,10 @@
+package api;
+
+import domain.users.UserRepository;
+
+public class FogTraelast {
+    private String VERSION = "0.1";
+    private final UserRepository userLists;
+    private final OrderRepository orderLists;
+
+}
