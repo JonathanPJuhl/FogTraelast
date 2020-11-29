1d4fe067c3a5d59bf79aaaa181a4ccd519ff0742
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2020-11-29 13:05:10 +0100

    fixed jsp issues

--- src/main/java/fogTraelast/web/pages/BaseServlet.java
+++ src/main/java/fogTraelast/web/pages/BaseServlet.java
@@ -12,25 +12,17 @@ public class BaseServlet extends HttpServlet {
 
     protected void render(String title, String content, HttpServletRequest req, HttpServletResponse resp) throws
             ServletException, IOException {
-        //resp.setContentType("text/html");
-        //resp.getWriter().println("<h1>Hello, World</h1>");
-        //req.setAttribute("navbar", new Navbar(req));
         req.setAttribute("title",title);
         req.setAttribute("content",content);
-        req.getRequestDispatcher("/WEB-INF/base.jsp").forward(req,resp);
+        req.getRequestDispatcher("/WEB-INF/pages/base.jsp").forward(req,resp);
     }
 
-   /* private static CupCakeAppRepository createCupCakeApp() throws IOException, SQLException {
-        Migrate.runMigrations();
-        return new CupCakeAppRepository(db, db, db);
-    }
-*/
-    protected void setup(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+   /* protected void setup(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
-    }
+    }*/
 
     protected void log(HttpServletRequest req, String message) {
-        System.err.println("(" + LocalDateTime.now() + "): " + this.getClass().getCanonicalName() + ": \"" + req.getRequestURI() + "\": " + message);
+        System.err.println("(" + LocalDateTime.now() + "): " + this.getClass() + ": \"" + req.getRequestURI() + "\": " + message);
     }
 }
