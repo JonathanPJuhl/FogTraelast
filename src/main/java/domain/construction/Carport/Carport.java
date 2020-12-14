30bd8b2c3aa9c49b24b0159ebdfb8e5bc7ef3e27
LKPDe <cph-lp262@cphbusiness.dk>
2020-12-14 16:35:23 +0100

    SVG Forfra, SVG oppefra WIP

---
+++ src/main/java/domain/construction/Carport/Carport.java
@@ -0,0 +1,8 @@
+package domain.construction.Carport;
+
+public class Carport {
+
+    public Carport(int height, int length, int width, String color) {
+        //super(height, length, width, String color);
+        }
+}
