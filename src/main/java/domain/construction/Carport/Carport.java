f1afd79a3633f2dce5e5593abd5922f4dab08443
JonathanPJuhl <49302239+JonathanPJuhl@users.noreply.github.com>
2020-12-17 10:03:58 +0100

    Delete Carport.java

--- src/main/java/domain/construction/Carport/Carport.java
+++
@@ -1,8 +0,0 @@
-package domain.construction.Carport;
-
-public class Carport {
-
-    public Carport(int height, int length, int width, String color) {
-        //super(height, length, width, String color);
-        }
-}
