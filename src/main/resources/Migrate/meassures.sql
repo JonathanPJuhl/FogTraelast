87edc08a376d1ca3430b8fd18da83fb6f851fbb5
Cathrine C <cph-ac221@cphbusiness.dk>
2021-01-02 12:06:00 +0100

    added measure script

---
+++ src/main/resources/Migrate/meassures.sql
@@ -0,0 +1,20 @@
+USE fogtraelast;
+
+
+DROP TABLE IF EXISTS lengths;
+DROP TABLE IF EXISTS widths;
+
+CREATE TABLE IF NOT EXISTS lengths(
+                                        length int PRIMARY KEY NOT NULL,
+                                        price double NOT NULL
+);
+INSERT INTO lengths(length, price) VALUES(3000, 70.00);
+INSERT INTO lengths(length, price) VALUES(6000, 120.00);
+
+CREATE TABLE IF NOT EXISTS widths(
+                                        width int PRIMARY KEY NOT NULL,
+                                        price double NOT NULL
+);
+
+INSERT INTO widths(width, price) VALUES(2090, 100.00);
+INSERT INTO widths(width, price) VALUES(1090, 90.00);
